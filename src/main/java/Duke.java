4f20dcd4083339cfc41175d643d68329fb89311b
ryanty <tanyuryan@gmail.com>
2019-08-17 16:52:39 +0800

    Implemented exceptions for (hopefully) all possible errors.
    Instructions handled:
    
    * No description given for Deadline for Event command
    * No timing given for Deadline for Event command
    * No description given for Todo command
    * Invalid command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,44 +4,62 @@ public class Duke {
     private static final String EXIT = "bye";
     private static TextList textList;
 
-    private static String[] splitString(String msg) {
+    private static String[] splitString(String msg) throws IllegalInstructionException {
         int i;
         String[] arr = msg.split("\\s+");
         for (i = 0; i < arr.length; i++)
             if (arr[i].startsWith("/"))
                 break;
-        StringBuilder taskDescription = new StringBuilder();
-        StringBuilder dateTime = new StringBuilder();
-        for (int j = 1; j < i; j++) 
-            taskDescription.append(arr[j] + " ");
-        for (int j = i + 1; j < arr.length; j++)
-            dateTime.append(arr[j] + " ");
-        String[] ret = {taskDescription.toString().trim(), dateTime.toString().trim()};
-        return ret;
+        if (i == arr.length - 1) { // nothing after /at or /by
+            throw new IllegalInstructionException("No timing given for " + arr[0] + " command.");
+        } else if (i == 1) { // nothing between command, or /at or /by
+            throw new IllegalInstructionException("No description given for " + arr[0] + " command.");
+        } else {
+            StringBuilder taskDescription = new StringBuilder();
+            StringBuilder dateTime = new StringBuilder();
+            for (int j = 1; j < i; j++) 
+                taskDescription.append(arr[j] + " ");
+            for (int j = i + 1; j < arr.length; j++)
+                dateTime.append(arr[j] + " ");
+            String[] ret = {taskDescription.toString().trim(), dateTime.toString().trim()};
+            return ret;
+        }
     }
 
     private static void handleAddingTasks(String msg) {
-        System.out.println("Got it. I've added this task:");
-        String command = msg.split("\\s+")[0];
-        Task t;
-        switch (command) {
-            case "todo":
-                t = new TodoTask(msg.substring("todo".length() + 1));
-                break;
-            case "deadline":
-                String[] infoDeadline = splitString(msg);
-                t = new DeadlineTask(infoDeadline[0], infoDeadline[1]);
-                break;
-            case "event":
-                String[] infoEvent = splitString(msg);
-                t = new EventTask(infoEvent[0], infoEvent[1]);
-                break;
-            default:
-                t = new Task(null);
+        try {
+            Task t;
+            String command = msg.split("\\s+")[0];
+            switch (command) {
+                case "todo":
+                    if (msg.split("\\s+").length <= 1) {
+                        throw new IllegalInstructionException(
+                                "The description of a todo cannot be empty");
+                    }
+                    t = new TodoTask(msg.substring("todo".length() + 1));
+                    break;
+
+                case "deadline":
+                    String[] infoDeadline = splitString(msg);
+                    t = new DeadlineTask(infoDeadline[0], infoDeadline[1]);
+                    break;
+
+                case "event":
+                    String[] infoEvent = splitString(msg);
+                    t = new EventTask(infoEvent[0], infoEvent[1]);
+                    break;
+
+                default:
+                    throw new IllegalInstructionException(
+                            "Sorry! I don't know what that means.");
+            }
+            textList.add(t);
+            System.out.println("Got it. I've added this task:");
+            System.out.println("\t" + t);
+            System.out.println("Now you have " + textList.size() + " tasks in the list.");
+        } catch (IllegalInstructionException e) {
+            System.out.println(e.toString());
         }
-        textList.add(t);
-        System.out.println("\t" + t);
-        System.out.println("Now you have " + textList.size() + " tasks in the list.");
     }
 
     /**
