229948fdc3fbb8ccf5aabdb31c2899cf8bc74491
ryanty <tanyuryan@gmail.com>
2019-08-19 09:24:56 +0800

    In the process of refactoring.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,29 +7,12 @@ public class Task {
         this.isDone = false;
     }
 
-    /**
-     * Returns a string containing a tick or cross, when this task is done
-     * or not done respectively.
-     */
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); // return tick or X symbols
-        //return (isDone ? "y" : "n"); // uses y/n as powershell has difficulty
-                                       // displaying unicode
     }
 
-    /**
-     * Marks this task as done.
-     */
     public void markAsDone() {
         this.isDone = true;
-    }
-
-    /**
-     * Returns this object as a string to save into a File.
-     * @return formatted string
-     */
-    public String formatAsData() {
-        return String.format("%s | %s", getStatusIcon(), description);
     }
 
     public String toString() {
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class TaskList {
+    /**
+     * The list of Tasks.
+     */
+    private ArrayList<Task> list;
+
+    /**
+     * The default max capacity, although this is not enforced.
+     */
+    private static final int MAX_ITEMS = 100;
+
+    /**
+     * Constructs an empty TaskList.
+     */
+    public TaskList() {
+        list = new ArrayList<Task>(MAX_ITEMS);
+    }
+
+    /**
+     * Returns the number of elements in this list.
+     * 
+     * @return the number of elements in this list
+     */
+    public int size() {
+        return this.list.size();
+    }
+
+    /**
+     * Appends the specified task to the end of this list.
+     * 
+     * @param t task to be appended to this list
+     */
+    public boolean add(Task t) {
+        list.add(t);
+        return true;
+    }
+
+    /**
+     * Marks the task at the specified index in the list as done. The specified index
+     * assumes a one-based list (i.e first element of the list is of index 1).
+     * 
+     * @param index the index of the element to be marked as done
+     */
+    public void done(int index) {
+        Objects.checkIndex(index - 1, list.size());
+        list.get(index - 1).markAsDone();
+    }
+
+    /**
+     * Returns the task at the specified index in the list. The specified index
+     * assumes a one-based list (i.e first element of the list is of index 1). The
+     * task is returned as a String.
+     * 
+     * @param index index at which the element is to be obtained.
+     * @return the String representation of the task at the specified index
+     */
+    public String get(int index) {
+        Objects.checkIndex(index - 1, list.size());
+        return list.get(index - 1).toString();
+    }
+
+    /**
+     * Removes and returns the task at the specified index in the list.
+     * 
+     * @param index index at which the element is to be removed
+     * @return the task that was removed
+     */
+    public Task remove(int index) {
+        Objects.checkIndex(index - 1, list.size());
+        return list.remove(index - 1);
+    }
+
+    /**
+     * Prints this Task list to System.out.
+     */
+    public void print() {
+        int i = 1;
+        for (Task t : list) {
+            System.out.println(String.format("%d. %s", i, t));
+            i++;
+        }
+    }
+    
+    /**
+     * Returns the string representation of this Task list.
+     * 
+     * @return the string representation of this Task list.
+     */
+    public String toString() {
+        if (list.isEmpty()) {
+            return "";
+        } else {
+            StringBuilder ret = new StringBuilder();
+            int i = 1;
+            for (Task t : list) {
+                ret.append(String.format("%d. %s\n", i, t));
+                i++;
+            }
+            return ret.toString();
+        }
+    }
+}
