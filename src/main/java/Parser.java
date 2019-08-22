523419358a89e18b39316eb29cb430cebbbf64a5
ryanty <tanyuryan@gmail.com>
2019-08-22 10:49:00 +0800

    Updated Parser.java

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,22 +1,29 @@
 public class Parser {
-
-
-    public static void parse(String input) throws IllegalInstructionException {
+    /**
+     * Handles parsing of user input.
+     * @param input
+     * @return a new Task represented by the input, null otherwise
+     * @throws IllegalInstructionException
+     */
+    public static Task parse(String input) throws IllegalInstructionException {
         String[] strings = input.split("\\s+");
         String command = strings[0];
         switch (command) {
             case "list":
+                return null;
 
             case "todo":
+                return TaskFactory.createTask(input);
 
             case "deadline":
-
             case "event":
+                
+                return TaskFactory.createTaskWithDate()
 
             case "done":
+                return null
 
             case "bye":
-
 
             default:
                 throw new IllegalInstructionException("Sorry! I don't know what that means.");
